1252e54c70528028b1267d13c5dcb770fc4c44e6
Sidak <sidak.pasrichassp@gmail.com>
2018-08-05 21:46:21 +0530

    Started on Navbar and added Responsive

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
+import 'font-awesome/css/font-awesome.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'mdbreact/dist/css/mdb.css';
 import './App.css';
+import NavbarFeatures from './navbarFeatures';
 
 class App extends Component {
   render() {
     return (
+      <div>
+        <NavbarFeatures />
       <div className="App">
         <header className="App-header">
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Random Text
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          Lol
+          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+          Lol
         </p>
+      </div>
       </div>
     );
   }
---
+++ src/navbarFeatures.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+import { Navbar, NavbarBrand, NavbarNav, NavbarToggler, Collapse, NavItem, NavLink, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'mdbreact';
+import { BrowserRouter as Router } from 'react-router-dom';
+
+class NavbarFeatures extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            collapse: false,
+            isWideEnough: false,
+            dropdownOpen: false
+        };
+    this.onClick = this.onClick.bind(this);
+    this.toggle = this.toggle.bind(this);
+    }
+
+    onClick(){
+        this.setState({
+            collapse: !this.state.collapse,
+        });
+    }
+
+    toggle() {
+        this.setState({
+            dropdownOpen: !this.state.dropdownOpen
+        });
+    }
+
+    render() {
+        return (
+            <Router>
+                <Navbar color="pink darken-4" dark expand="md" sticky="top">
+                    { !this.state.isWideEnough && <NavbarToggler onClick = { this.onClick } />}
+                    <Collapse isOpen = { this.state.collapse } navbar>
+                        <NavbarNav center>
+                          <NavItem active>
+                              <NavLink to="#">Home</NavLink>
+                          </NavItem>
+                          <NavItem>
+                              <NavLink to="#">Features</NavLink>
+                          </NavItem>
+                          <NavbarBrand href="/">
+                            <img src="./techsoc.png" height="40 dp"/>
+                          </NavbarBrand>
+                          <NavItem>
+                              <NavLink to="#">Pricing</NavLink>
+                          </NavItem>
+                          <NavItem>
+                            <NavLink to="#">Pricing</NavLink>
+                          </NavItem>
+                        </NavbarNav>
+                    </Collapse>
+                </Navbar>
+            </Router>
+        );
+    }
+}
+
+export default NavbarFeatures;
