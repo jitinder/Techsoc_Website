0e8449e1fce4cb6acdc7e2c67eb34c07d30c35b3
Sidak <sidak.pasrichassp@gmail.com>
2018-08-14 00:17:12 +0530

    Made initial App Header final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import 'font-awesome/css/font-awesome.min.css';
-import 'bootstrap/dist/css/bootstrap.min.css'; 
+import 'bootstrap/dist/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 import './App.css';
 import NavbarFeatures from './navbarFeatures';
@@ -10,18 +10,27 @@ class App extends Component {
     return (
       <div>
         <NavbarFeatures />
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to the page</h1>
-        </header>
-        <p className="App-intro">
-          Random Text
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          Lol
-          <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
-          Lol
+        <div className="App">
+          <header className="App-header">
+            <div class="container flex-center">
+              <div class="row pt-5 mt-3">
+                <div class="col-md-12">
+                  <h1 class="h1-responsive white-text text-uppercase mb-3">UCL Technology Society</h1>
+                  <hr class="hr-light mt-4"/>
+                  <br/>
+                  <h6 class="mb-6 white-text">Promoting technological awareness and innovation both within and beyond UCL</h6>
+                </div>
+              </div>
+            </div>
+          </header>
+          <p className="App-intro">
+            Random Text
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            Lol
+          <br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />
+            Lol
         </p>
-      </div>
+        </div>
       </div>
     );
   }
--- src/navbarFeatures.js
+++ src/navbarFeatures.js
@@ -27,12 +27,13 @@ class NavbarFeatures extends React.Component {
     }
 
     render() {
-        const view = {background: 'url("https://i.stack.imgur.com/0ASXA.jpg")no-repeat center center', backgroundSize: 'cover', height: '100vh'}
+        const view = {background: 'url(./bg.png)no-repeat center center', backgroundSize: 'cover', height: '100vh'}
         return (
             <div>
                 <header>
             <Router>
                 <Navbar color="transparent" dark expand="md" fixed="top" scrolling>
+                <Container>
                     <NavbarBrand href="/">
                         <img src="./techsoc_logo.svg" height="50 dp" alt="TechSoc Logo"/>
                     </NavbarBrand>
@@ -40,48 +41,44 @@ class NavbarFeatures extends React.Component {
                     <Collapse isOpen = { this.state.collapse } navbar>
                         <NavbarNav right>
                           <NavItem style={{paddingTop: 10}}>
-                              <NavLink to="#">About</NavLink>
+                              <NavLink to="#"><b>About</b></NavLink>
                           </NavItem>
                           <NavItem style={{paddingTop: 10}}>
-                              <NavLink to="#">Events</NavLink>
+                              <NavLink to="#"><b>Events</b></NavLink>
                           </NavItem>
                           <NavItem style={{paddingTop: 10}}>
-                              <NavLink to="#">Join</NavLink>
+                              <NavLink to="#"><b>Sponsors</b></NavLink>
                           </NavItem>
-                        </NavbarNav>
-                        <Desktop>
-                        <NavbarNav right>
+                          <NavItem style={{paddingTop: 10}}>
+                              <NavLink to="#"><b>Contact Us</b></NavLink>
+                          </NavItem>
+                        {/*<Desktop>
                         <NavItem style={{paddingTop: 10}}>
-                              <NavLink to="#"><i class="fa fa-envelope fa-lg white-text" aria-hidden="true"></i></NavLink>
+                              <NavLink to="#"><i class="fa fa-envelope fa-lg" aria-hidden="true"></i></NavLink>
                           </NavItem>
                           <NavItem style={{paddingTop: 10}}>
-                              <NavLink to="#"><i class="fa fa-facebook-official fa-lg indigo-text" aria-hidden="true"></i></NavLink>
+                              <NavLink to="#"><i class="fa fa-facebook-official fa-lg" aria-hidden="true"></i></NavLink>
                           </NavItem>
                           <NavItem style={{paddingTop: 10}}>
-                              <NavLink to="#"><i class="fa fa-twitter-square fa-lg cyan-text" aria-hidden="true"></i></NavLink>
+                              <NavLink to="#"><i class="fa fa-twitter-square fa-lg" aria-hidden="true"></i></NavLink>
                           </NavItem>
                           <NavItem style={{paddingTop: 10}}>
                             <NavLink to="#"><i class="fa fa-instagram fa-lg" aria-hidden="true"></i></NavLink>
                           </NavItem>
                           <NavItem style={{paddingTop: 10}}>
-                            <NavLink to="#"><i class="fa fa-snapchat-square fa-lg yellow-text" aria-hidden="true"></i></NavLink>
+                            <NavLink to="#"><i class="fa fa-snapchat-square fa-lg" aria-hidden="true"></i></NavLink>
                           </NavItem>
+                          
+                        </Desktop>*/}
                         </NavbarNav>
-                        </Desktop>
                     </Collapse>
+                    </Container>
                 </Navbar>
             </Router>
-
-            <View style={view}>
-            <Mask overlay="purple-light" style={{flexDirection: 'column'}} className="flex-center text-white text-center">
-                <h2>This Navbar is fixed</h2>
-                <h5>It will always stay visible on the top, even when you scroll down</h5>
-                <p>Navbar's background will switch from transparent to solid color while scrolling down</p><br/>
-                <p>Full page intro with background image will be always displayed in full screen mode, regardless of device </p>
-              </Mask>
-              </View>
               </header>
+              
             </div>
+            
         );
     }
 }
